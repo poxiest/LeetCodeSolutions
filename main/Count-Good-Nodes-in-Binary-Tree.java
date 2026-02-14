a56675c3a1c0288c98be163bc2c5d1e4e4303138
Vishaak R <59653535+poxiest@users.noreply.github.com>
2026-02-14 11:57:20 -0600

    [1448] [Time Beats: 93.11%] [Memory Beats: 12.15%] - LeetPush

--- main/Count-Good-Nodes-in-Binary-Tree.java
+++ main/Count-Good-Nodes-in-Binary-Tree.java
@@ -18,14 +18,16 @@
 18        return dfs(root, root.val);
 19    }
 20
-21    private int dfs(TreeNode root, int max) {
-22        if (root == null)
-23            return 0;
-24
-25        int res = root.val >= max ? 1 : 0;
-26        max = Math.max(max, root.val);
-27        res += dfs(root.left, max);
-28        res += dfs(root.right, max);
-29        return res;
-30    }
-31}
+21    // since max is local, it will hold the value like a stack, so at 
+22    // any moment it is the actual max in that path
+23    private int dfs(TreeNode root, int max) {
+24        if (root == null)
+25            return 0;
+26
+27        int res = root.val >= max ? 1 : 0;
+28        max = Math.max(max, root.val);
+29        res += dfs(root.left, max);
+30        res += dfs(root.right, max);
+31        return res;
+32    }
+33}
