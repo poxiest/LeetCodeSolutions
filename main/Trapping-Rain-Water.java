f48cedabaa5ae2f33cc93f3bfbcc973ebe7183fa
Vishaak R <59653535+vishaakr98@users.noreply.github.com>
2026-03-12 17:40:04 -0500

    [42] [Time Beats: 59.93%] [Memory Beats: 33.09%] - LeetPush

--- main/Trapping-Rain-Water.java
+++ main/Trapping-Rain-Water.java
@@ -1,21 +1,25 @@
 1class Solution {
 2    public int trap(int[] height) {
-3        int n = height.length;
-4        int[] l = new int[n];
-5        int[] r = new int[n];
-6        l[0] = height[0];
-7        r[n-1] = height[n-1];
-8        int ans = 0;
-9
-10        for(int i = 1 ; i < n ; i++)
-11            l[i] = Math.max(l[i-1], height[i]);
-12        
-13        for(int i = n-2 ; i >= 0 ; i--)
-14            r[i] = Math.max(r[i+1], height[i]);
-15
-16        for(int i = 0 ; i < n ; i++)
-17            ans += Math.min(l[i], r[i]) - height[i]; 
-18        
-19        return ans;
-20    }
-21}
+3        int res = 0;
+4        int lHeight[] = new int[height.length];
+5        int rHeight[] = new int[height.length];
+6
+7        // maintians max left height
+8        lHeight[0] = height[0];
+9        for(int i = 1 ; i < height.length ; i++) {
+10            lHeight[i] = Math.max(lHeight[i-1], height[i]);
+11        }
+12
+13        // maintain max right height
+14        rHeight[height.length-1] = height[height.length-1];
+15        for(int i = height.length-2 ; i >= 0  ; i--) {
+16            rHeight[i] = Math.max(rHeight[i+1], height[i]);
+17        }
+18
+19        for(int i = 0 ; i < height.length ; i++) {
+20            res += Math.min(lHeight[i], rHeight[i]) - height[i];
+21        }
+22
+23        return res;
+24    }
+25}
